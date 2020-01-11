6cf26ee54b76d21147beec553d0f7552ece93da0
Lucas Sousa <lucasmag97@gmail.com>
2020-01-10 22:54:46 -0300

    Correção de host

--- back/app/util/consts.py
+++ back/app/util/consts.py
@@ -2,7 +2,7 @@ API_KEY = 'JD3SCYOQC63YYZQG'
 
 #host = locahost | database
 DATABASE_URL = "postgres://{user}:{password}@{host}:{port}/{database}".format(
-        user='bovespaapp', password='bovespaapp', host='0.0.0.0',
+        user='bovespaapp', password='bovespaapp', host='database',
         port=5432, database='bovespaapp')
 
 BOVESPA_SYMBOL = "^BVSP"
